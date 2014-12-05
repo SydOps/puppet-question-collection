2b9807944961bfe22da048fc1416b937df9dd746
Bill W <SydOps@users.noreply.github.com>
2014-12-05 12:46:33 +1100

    Update init.pp

--- modules/appserver/manifests/init.pp
+++ modules/appserver/manifests/init.pp
@@ -1,3 +1,9 @@
 class appserver {
   include appserver::params
+  
+  # Testing only, real path should be something like: WEB-INF/conf/axis2.xml
+  file { '/etc/axis2.xml':
+      ensure => file;
+      content => template("appserver/axis2.xml.erb")
+  }
 }
